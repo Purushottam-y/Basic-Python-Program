4dbbff3613e7e4a2c40638e8280cf017b375f60b
Purushottam-y <purushottamkumarraj6287@gmail.com>
2025-07-24 10:18:36 +0530

    Added Task 1 and 2 Python file

---
+++ scratch.py
@@ -0,0 +1,48 @@
+# Task 1 : Perform Basic Mathematical Operations
+# Problem Statement: Wirte a Python Program that does the folling:
+# 1 Takes two numbers as input from the use.
+
+'''a=input("Enter the first number: ")
+b=input("Enter the second number: ")'''
+
+# Performs the basic mathematical operations on these two numbers:
+
+'''a=input("Enter the first number :")
+b=input("Enter the second number :")
+a=int(a)
+b=int(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("Sum :",c,"Substraction",d,"multiplicaion",e,"division",f)'''
+
+#Displays the results of each operation on the screen.
+
+'''a=input("Enter the first number :")
+b=input("Enter the second number :")
+a=int(a)
+b=int(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("Sum :",c,"Substraction",d,"multiplicaion",e,"division",f)'''
+
+# Task 2: Create a Personlized Greating
+# Problem Statement: Write a Python program that:
+# 1. Takes a user's first name and last name as input.
+'''a=input("Enter the first Name")
+b=input("Enter the Last Name")'''
+
+# 2. Concatenates the first name and last name into a full name.
+'''a=input("Enter the Fisrt Name :")
+b=input("Enter the Last Name :")
+print("Full Name :",a,b)'''
+
+# 3. Prints a Personalized greating message using the full name.
+a=input("Enter the First Name ")
+b=input("Enter the Last Name ")
+print("Hello,",a,b,"Welcome to the Python program.")
+
+
